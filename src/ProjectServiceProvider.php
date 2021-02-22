061c3e3b7edfa746390f7b157cd3fca979761707
nipunnoushad <nipun@tritiyo.com>
2021-02-22 12:46:47 +0600

    BugFix

--- src/ProjectServiceProvider.php
+++ src/ProjectServiceProvider.php
@@ -9,9 +9,7 @@ class ProjectServiceProvider extends ServiceProvider {
 
     public function boot()
     { 
-        //Route
         $this->loadRoutesFrom(__DIR__. '/routes/modules/projects.php');
-        //All View File 
         $this->loadViewsFrom(__DIR__. '/views', 'project');
         $this->loadMigrationsFrom(__DIR__. '/database/migrations');
     }
